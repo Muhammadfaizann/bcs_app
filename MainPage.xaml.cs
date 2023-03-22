91bae62bf4a4ae3b086c0171e8d6c4ede56e065d
Ravi verma <rverma@marksnelsoncpa.com>
2023-03-22 20:28:43 +0530

    design update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,51 +2,13 @@
 
 using IronPython.Hosting;
 
-
-
-
 public partial class MainPage : ContentPage
 {
     public int label_font_size = 10;
 
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-
+    public MainPage()
+    {
+        InitializeComponent();
     }
-
-
-    //private async void PrintImageCommand(object sender, EventArgs e)
-    //{
-    //    // Your code to print the image here
-    //    await DisplayAlert("Print Image", "Image printed", "OK");
-    //}
-
-
-    //private void OnCounterClicked(object sender, EventArgs e)
-    //{
-    //    count++;
-    //    var py = Python.CreateEngine();
-    //    try
-    //    {
-    //        py.ExecuteFile("Test.py");
-            
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Console.WriteLine(ex.Message.ToString());
-    //    }
-
-    //    if (count == 1)
-    //        CounterBtn.Text = $"Clicked {count} time {py.Execute("print('hello from python')")}";
-    //    else
-    //        CounterBtn.Text = $"Clicked {count} times {py.Execute("print('hello from python')")}";
-
-    //    SemanticScreenReader.Announce(CounterBtn.Text);
-    //}
-
-
 }
 
