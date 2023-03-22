003c41ad052b9bdc7b2ce886f434097fe757c140
Ravi verma <rverma@marksnelsoncpa.com>
2023-03-23 01:07:28 +0530

    app size

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,10 +2,25 @@
 
 public partial class App : Application
 {
-	public App()
+    const int WindowWidth = 400;
+    const int WindowHeight = 300;
+    public App()
 	{
 		InitializeComponent();
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 1320;
+        const int newHeight = 740;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
